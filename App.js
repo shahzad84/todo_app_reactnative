59fa594e3b241ab3663af365e63b8895fce4c857
hilarious3 <shahzad20022002@gmail.com>
2023-09-12 11:45:17 +0530

    changing state of person using usestate

--- App.js
+++ App.js
@@ -4,15 +4,18 @@ import {React,useState} from 'react';
 import { Button } from 'react-native-web';
 export default function App() {
   const [name,setName]=useState("shahzad");
+  const [person,setPerson]=useState({name:"cool",age:"34"});
   const clickHandle=()=>{
     setName("good");
+    setPerson({name:"noob",age:"53"})
   }
   return (
     <View style={styles.container}>
       <Text>hii my name is {name}</Text>
-      <view>
+      <Text>this is {person.name} and my age is {person.age} </Text>
+      <View>
         <Button title="click" onPress={clickHandle}/>
-      </view>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
